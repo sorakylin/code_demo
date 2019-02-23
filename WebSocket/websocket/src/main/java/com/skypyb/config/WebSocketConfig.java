c9d56d5f361b4a8edad61cea40b9da45ab33f7c7
skypyb <pyb068@gmail.com>
2019-02-23 16:00:18 +0800

    【WebSocket】 @ServerEndpoint 注解方式的 WebSocket 开发演示

---
+++ WebSocket/websocket/src/main/java/com/skypyb/config/WebSocketConfig.java
@@ -0,0 +1,21 @@
+package com.skypyb.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+@Configuration
+public class WebSocketConfig {
+
+    /**
+     * 这个bean会自动注册使用了@ServerEndpoint注解声明的对象
+     * 没有的话会报404
+     *
+     * @return
+     */
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+
+        return new ServerEndpointExporter();
+    }
+}
