83f452510535fa800be20c8502eda578b4a870e1
skypyb <pyb068@gmail.com>
2019-02-27 20:01:48 +0800

    【Event】Module created.

---
+++ Event-Springboot/src/main/java/com/skypyb/event/EventApplication.java
@@ -0,0 +1,12 @@
+package com.skypyb.event;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventApplication.class, args);
+    }
+}
