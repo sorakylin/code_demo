be3cc05f9a7f14ab2fe7e4ac8da31f1d3b16eca7
skypyb <pyb068@gmail.com>
2019-05-08 16:18:08 +0800

    【Cache】Module created.

---
+++ Cache-SpringBoot/src/main/java/com/skypyb/CacheDemoApplication.java
@@ -0,0 +1,11 @@
+package com.skypyb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacheDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CacheDemoApplication.class, args);
+    }
+}
