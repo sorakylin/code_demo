22e62aaddfa14a2b7258c276b8ceb33ff5ef4642
skypyb <pyb068@gmail.com>
2019-05-16 12:56:16 +0800

    【SpringCloud】 服务消费者使用 DiscoveryClient 动态获取了服务提供者的地址。

--- sc-demo-microservice/sc-demo-microservice-movie/src/main/java/com/skypyb/sc/controller/MovieController.java
+++ sc-demo-microservice/sc-demo-microservice-movie/src/main/java/com/skypyb/sc/controller/MovieController.java
@@ -4,11 +4,15 @@ import com.skypyb.sc.entity.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/movie")
@@ -20,9 +24,20 @@ public class MovieController {
     private RestTemplate restTemplate;
 
 
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+
     @GetMapping("/{id}")
     public User getUser(@PathVariable Long id) {
+
+        //获取 user 服务的信息
+        List<ServiceInstance> instances =
+                discoveryClient.getInstances("sc-demo-microservice-user");
+
+        String userUrl = instances.get(0).getUri().toString();
+
         logger.info("access getUser method.");
-        return restTemplate.getForObject("http://localhost:8088/user/" + id, User.class);
+        return restTemplate.getForObject(userUrl + "/user/" + id, User.class);
     }
 }
