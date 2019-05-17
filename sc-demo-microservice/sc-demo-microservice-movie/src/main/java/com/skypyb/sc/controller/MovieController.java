e18231ee48fd3befeb22716d607d40654f6d1f54
skypyb <pyb068@gmail.com>
2019-05-17 09:09:41 +0800

    【SpringCloud】 使用 Ribbon 实现了调用服务的负载均衡

--- sc-demo-microservice/sc-demo-microservice-movie/src/main/java/com/skypyb/sc/controller/MovieController.java
+++ sc-demo-microservice/sc-demo-microservice-movie/src/main/java/com/skypyb/sc/controller/MovieController.java
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,19 +26,18 @@ public class MovieController {
 
 
     @Autowired
-    private DiscoveryClient discoveryClient;
+    private LoadBalancerClient loadBalancerClient;
 
 
     @GetMapping("/{id}")
     public User getUser(@PathVariable Long id) {
 
-        //获取 user 服务的信息
-        List<ServiceInstance> instances =
-                discoveryClient.getInstances("sc-demo-microservice-user");
+        //获取 user 服务的信息,使用到了 Ribbon 实现负载均衡
+        ServiceInstance choose = loadBalancerClient.choose("sc-demo-microservice-user");
 
-        String userUrl = instances.get(0).getUri().toString();
+        String userUrl = choose.getUri().toString();
 
-        logger.info("access getUser method.");
+        logger.info("access getUser method , service info: {},{}", choose.getServiceId(), userUrl);
         return restTemplate.getForObject(userUrl + "/user/" + id, User.class);
     }
 }
