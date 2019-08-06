f84f6d6d3b82234075902b3edc468a162d364f2b
skypyb <pyb068@gmail.com>
2019-08-06 17:26:52 +0800

    【dubbo-springboot】项目构建

---
+++ dubbo-springboot/dubbo-user-service-api/src/main/java/com/skypyb/dubbo/service/user/api/UserService.java
@@ -0,0 +1,7 @@
+package com.skypyb.dubbo.service.user.api;
+
+public interface UserService {
+
+    String hello();
+
+}
---
+++ dubbo-springboot/dubbo-user-service-provider/src/main/java/com/skypyb/dubbo/user/service/provider/DubboUserServiceProviderApplication.java
@@ -0,0 +1,13 @@
+package com.skypyb.dubbo.user.service.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubboUserServiceProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DubboUserServiceProviderApplication.class, args);
+	}
+
+}
