98afabb0593d5bb046f8ad7bc4c2ed0eb95577ce
skypyb <pyb068@gmail.com>
2019-08-06 21:19:59 +0800

    【dubbo-springboot】实现负载均衡的功能(轮循)

--- dubbo-springboot/dubbo-user-service-provider/src/main/java/com/skypyb/dubbo/user/service/provider/api/impl/UserServiceImpl.java
+++ dubbo-springboot/dubbo-user-service-provider/src/main/java/com/skypyb/dubbo/user/service/provider/api/impl/UserServiceImpl.java
@@ -2,12 +2,21 @@ package com.skypyb.dubbo.user.service.provider.api.impl;
 
 import com.alibaba.dubbo.config.annotation.Service;
 import com.skypyb.dubbo.service.user.api.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 
 @Service(version = "${user.service.version}")
 public class UserServiceImpl implements UserService {
 
+    private Logger logger = LoggerFactory.getLogger(UserService.class);
+
+    @Value("${dubbo.protocol.port}")
+    private String port;
+
     @Override
     public String hello() {
+        logger.info("access hello, service port:{}", this.port);
         return "hello dubbo";
     }
 
