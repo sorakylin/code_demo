82deb0adb175307b23c710b511b45873f52d2dad
skypyb <pyb068@gmail.com>
2019-08-06 19:49:51 +0800

    【dubbo-springboot】构建消费者项目、初始依赖

---
+++ dubbo-springboot/dubbo-user-service-consumer/src/main/java/com/skypyb/dubbo/user/service/consumer/DubboUserServiceConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.skypyb.dubbo.user.service.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubboUserServiceConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DubboUserServiceConsumerApplication.class, args);
+	}
+
+}
