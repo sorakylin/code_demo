d207615e75fc02d8e33f7e2eff315e56a42b02a8
skypyb <pyb068@gmail.com>
2019-08-07 10:17:55 +0800

    【dubbo-springboot】增加 Hystrix 仪表盘监控

--- dubbo-springboot/dubbo-user-service-consumer/src/main/java/com/skypyb/dubbo/user/service/consumer/DubboUserServiceConsumerApplication.java
+++ dubbo-springboot/dubbo-user-service-consumer/src/main/java/com/skypyb/dubbo/user/service/consumer/DubboUserServiceConsumerApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
-@EnableHystrix
 @SpringBootApplication
 public class DubboUserServiceConsumerApplication {
 
---
+++ dubbo-springboot/dubbo-user-service-consumer/src/main/java/com/skypyb/dubbo/user/service/consumer/config/HystrixConfig.java
@@ -0,0 +1,31 @@
+package com.skypyb.dubbo.user.service.consumer.config;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@EnableHystrix
+@EnableHystrixDashboard
+@Configuration
+public class HystrixConfig {
+
+
+    /**
+     * 仪表盘
+     *
+     * @return
+     */
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
+
+}
