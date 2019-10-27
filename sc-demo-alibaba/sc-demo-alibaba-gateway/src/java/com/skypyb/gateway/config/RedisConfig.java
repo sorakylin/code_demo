6ca980ba1f91239764cfcd0b7dcafdfb5cc4c1a2
skypyb <pyb068@gmail.com>
2019-10-27 10:01:29 +0800

    【sc-demo-alibaba】 Gateway限流，错误配置修复

--- sc-demo-alibaba/sc-demo-alibaba-gateway/src/java/com/skypyb/gateway/config/RedisConfig.java
+++ sc-demo-alibaba/sc-demo-alibaba-gateway/src/java/com/skypyb/gateway/config/RedisConfig.java
@@ -2,6 +2,7 @@ package com.skypyb.gateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
@@ -12,13 +13,14 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 public class RedisConfig {
 
     @Bean
-    public RedisTemplate<String, Object> redisTemplate(JedisConnectionFactory connectionFactory) {
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
         redisTemplate.setKeySerializer(stringRedisSerializer);
         redisTemplate.setHashKeySerializer(stringRedisSerializer);
         redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<Object>(Object.class));
         redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+        redisTemplate.setConnectionFactory(connectionFactory);
         return redisTemplate;
     }
 }
