6a06c798466133aa21ce18e89edf81c618518329
skypyb <pyb068@gmail.com>
2020-06-07 16:29:05 +0800

    [sharding-jdbc] completion

--- shardingsphere-springboot/sharding-jdbc/src/main/java/com/skypyb/demo/sharding/ShardingJDBCApplication.java
+++ shardingsphere-springboot/sharding-jdbc/src/main/java/com/skypyb/demo/sharding/ShardingJDBCApplication.java
@@ -2,8 +2,10 @@ package com.skypyb.demo.sharding;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
+@MapperScan(value = {"com.skypyb.demo.sharding.**.dao"})
 public class ShardingJDBCApplication {
 
     public static void main(String[] args) {
---
+++ shardingsphere-springboot/sharding-jdbc/src/main/java/com/skypyb/demo/sharding/order/dao/OrderMapper.java
@@ -0,0 +1,7 @@
+package com.skypyb.demo.sharding.order.dao;
+
+import com.skypyb.demo.sharding.order.entity.OrderDO;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface OrderMapper extends Mapper<OrderDO> {
+}
